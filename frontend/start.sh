f533e2a8614188d399618629382c5db7e6367b32
waheedcheema77 <abdulwaheedcheema77@gmail.com>
2026-08-31 05:14:32 +0500

    Fix venv activation in start.sh for Nixpacks

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd scripts
+if [ -d "/app/.venv" ]; then
+  source /app/.venv/bin/activate
+fi
 python -m uvicorn server:app --host 0.0.0.0 --port 8000 &
 cd ..
 npx next start -p $PORT
